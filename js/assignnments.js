f2fd1dba73f34e036f95a2f9871711d66b6728c7
abdalrhmanalshyk <97473097+abdalrhman11996@users.noreply.github.com>
2022-08-21 23:42:47 +0300

    learnjavaScript

---
+++ js/assignnments.js
@@ -0,0 +1,89 @@
+/*1 > 9*/
+// document.write("<h1>Elzero</h1>");
+
+// document.querySelector("h1").style.color = "blue";
+// document.querySelector("h1").style.fontSize = "80px";
+// document.querySelector("h1").style.textAlign = "center";
+// document.querySelector("h1").style.fontFamily = "Arial";
+
+// console.log("%cElzero %cWeb %cSchool", "font-size: 40px; color:red","color:green; font-weight: bold; font-size: 40px","font-size:40px; color:white; background: blue;");
+
+
+// console.group("group 1");
+// console.log("massge 1");
+// console.log("massge 2");
+// console.group("child group 1");
+// console.log("massge 1");
+// console.log("massge 2");
+// console.group("grand child group 1");
+// console.log("massge 1");
+// console.log("massge 2");
+// console.groupEnd();
+// console.groupEnd();
+// console.groupEnd();
+// console.group("group 2");
+// console.log("massge 1");
+// console.log("massge 2");
+
+// console.table(["Ahmed", "Khaled", "Morad", "Jafer", "Sadek", "Kaseem"]);
+
+// console.log("Iam In Console"); // stoped comment
+// document.write("Iam In Page"); // stoped comment
+
+
+/*
+console.log("Iam In Console"); // stoped comment
+document.write("Iam In Page"); // stoped comment
+
+*/
+
+/*17*/
+
+// let theTitle = "Elzero", pageDescraption="Elzero Web School", pageDate="5/25";
+
+// let card =   
+          
+//           `<div> 
+//             <h3>${theTitle}</h3>
+//             <p>${pageDescraption}</p>
+//             <span>${pageDate}</span>
+//           </div>`;
+// document.write(card.repeat(4));
+
+/*10 > 17*/
+
+
+// let numberOne = 10;
+// let numberTow = 20;
+
+// console.log(numberOne + "" + numberTow);
+// console.log(typeof (numberOne + "" + numberTow));
+// console.log(`${numberOne}${numberTow}`);
+// console.log(typeof`${numberOne}  ${numberTow}`);
+
+// console.log(numberTow + "\n" +numberOne);
+// console.log(`${numberTow}
+// ${numberOne} `);
+
+// console.log(elzero.innerHTML); // object
+// console.log(typeof elzero); // object
+
+// console.log("`I\'m In \n \\\\ \n Love \\\\ \"\"\" \'\'\' \n \+\+ With \+\+ \n \\\"\"\"\\\"\"\" \n \"\"Javascript\"\"`` ");
+
+// console.log(`I'm In
+// \\\\ Love \\\\ """ '''' 
+// ++ With ++
+// \\"""\\'''
+// "" Javascript ""\`\` `);
+
+// let a = 21;
+// let b = 20;
+// let c = `${b}${a}`;
+// console.log(`_${a}_${c}_${c}_${c}_${b}`);// _21_2021_2021_2021_20_;
+// let a = 21;
+// let b = 20;
+// let c=`${b}${a}_`;
+
+// result=c.repeat(3);
+
+// console.log(`_${a}_${result}_${b}`);     
---
+++ js/index.js
@@ -0,0 +1,22 @@
+// window.onload = function  () {
+//    document.querySelector("h1").style.color = "red";
+// }
+
+// window.onload = function () {
+//    document.querySelector("h1").style.color = "blue"
+// }
+
+
+// window.onload = function () {
+//    document.querySelector("h1").style.color = "red"
+// }
+
+// window.onload = function () {
+//    document.querySelector("h1").style.color = "red"
+// }
+
+
+window.onload = function () {
+   document.querySelector("h1").style.color = "blue";
+   document.querySelector('h2').style.color = 'red';
+};
